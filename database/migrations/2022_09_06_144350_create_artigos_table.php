cbb80495246eb0b5c8903262d364e99682c083da
Rafael Batalha <raf.bsiufpa@gmail.com>
2022-09-06 15:09:31 -0300

    migration artigos

---
+++ database/migrations/2022_09_06_144350_create_artigos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artigos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo')->unique();
+            $table->text('conteudo');
+            $table->string('imagem')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artigos');
+    }
+};
