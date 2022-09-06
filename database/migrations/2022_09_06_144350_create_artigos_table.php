104037be16d100b1bbf9ec1bc74deda9ee48f74b
Rafael Batalha <raf.bsiufpa@gmail.com>
2022-09-06 16:11:48 -0300

    Alterações migration artigos

--- database/migrations/2022_09_06_144350_create_artigos_table.php
+++ database/migrations/2022_09_06_144350_create_artigos_table.php
@@ -27,10 +27,6 @@ return new class extends Migration
                 ->on('users')
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
-                
-
-
-
         });
     }
 
